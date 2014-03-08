577e7fbe98b13104e894b5588b63207e13ac24d0
Adam Snetman <adam@startingnow.co>
2014-03-08 17:31:25 -0500

    Fix some pointers to ruby custom plugins/installs;

--- compass/config.rb
+++ compass/config.rb
@@ -3,10 +3,8 @@ project_path=File.expand_path("..",File.dirname(__FILE__))
 
 # We also support plugins and frameworks, please read the docs http://docs.mixture.io/preprocessors#compass
 # Require any additional compass plugins here.
-require "/Users/snetman/.rvm/gems/ruby-1.9.3-p286/gems/breakpoint-2.0.7/lib/breakpoint.rb"
-require "/Users/snetman/.rvm/gems/ruby-1.9.3-p286/gems/singularitygs-1.1.2/lib/singularitygs.rb"
-# The singularity require above kept failing until I edited the path to breakpoint in the singularity.rb gemfile above
-# See here for more info: http://support.mixture.io/discussions/problems/259-compass-cant-find-plugins#comment_26684642
+require "breakpoint"
+require "singularitygs"
 
 # Important! change the paths below to match your project setup
 css_dir = "public/styles" # update to the path of your css files.
